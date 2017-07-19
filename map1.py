1e93caafd13eee807b17572d2a65ba63ab0bf08a
YiTingLee <ranye0719@gmail.com>
2017-07-19 16:50:24 +0800

    add Layer Control && finish

--- map1.py
+++ map1.py
@@ -16,17 +16,21 @@ def color_porducer(elevation):
 
 map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
 
-fg = folium.FeatureGroup(name = "My Map")
+fgmarker = folium.FeatureGroup(name = "Marker Layer")
 
 # for lt, ln, el in zip(lat, lon, elev):
 #     fg.add_child(folium.Marker(location=[lt, ln], popup = str(el) + " m", icon = folium.Icon(color=color_porducer(el))))
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+ " m", fill_color=color_porducer(el), color='grey', fill_opacity=0.7))
+    fgmarker.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+ " m", fill_color=color_porducer(el), color='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'),
+fgpolygon = folium.FeatureGroup(name = "Polygon Layer")
+fgpolygon.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'),
 style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005'] < 10000000
 else 'blue' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+map.add_child(fgmarker)
+map.add_child(fgpolygon)
+map.add_child(folium.LayerControl())
+
 map.save("Map1.html")
