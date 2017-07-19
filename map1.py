120604b28836ec1b707477a99fcdd6a353cecfc8
YiTingLee <ranye0719@gmail.com>
2017-07-19 16:09:53 +0800

    polygon layer

--- map1.py
+++ map1.py
@@ -18,9 +18,13 @@ map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name = "My Map")
 
+# for lt, ln, el in zip(lat, lon, elev):
+#     fg.add_child(folium.Marker(location=[lt, ln], popup = str(el) + " m", icon = folium.Icon(color=color_porducer(el))))
+
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup = str(el) + " m", icon = folium.Icon(color=color_porducer(el))))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+ " m", fill_color=color_porducer(el), color='grey', fill_opacity=0.7))
+
+fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig'))))
 
 map.add_child(fg)
-
 map.save("Map1.html")
