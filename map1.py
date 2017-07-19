87594640cbb33c8102a464ed49fc296cd94625fc
YiTingLee <ranye0719@gmail.com>
2017-07-19 15:47:18 +0800

    add color

--- map1.py
+++ map1.py
@@ -4,13 +4,22 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_porducer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000<= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
 
 map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name = "My Map")
 
-for lt,ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup = "Hi I am a Marker", icon = folium.Icon(color='blue')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup = str(el) + " m", icon = folium.Icon(color=color_porducer(el))))
 
 map.add_child(fg)
 
