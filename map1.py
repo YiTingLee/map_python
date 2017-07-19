f3b1f52315ffb7dda18dcae86a433ba0bfe360be
YiTingLee <ranye0719@gmail.com>
2017-07-19 16:39:54 +0800

    color-based polygon feature

--- map1.py
+++ map1.py
@@ -24,7 +24,9 @@ fg = folium.FeatureGroup(name = "My Map")
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+ " m", fill_color=color_porducer(el), color='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig'))))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'),
+style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005'] < 10000000
+else 'blue' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
 map.save("Map1.html")
